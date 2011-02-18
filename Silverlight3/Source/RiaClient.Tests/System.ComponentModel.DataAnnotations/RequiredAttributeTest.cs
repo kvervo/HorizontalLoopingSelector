8d99e71aa3aee0c5d6e115c1af4817dc54193c50
SND\jeffwilcox_cp <SND\jeffwilcox_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2011-02-18 00:22:14 +0000

    Silverlight for Windows Phone Toolkit Feb 2011 - Fixing a customer-reported ListPicker issues when returning
    git-svn-id: https://Silverlight.svn.codeplex.com/svn/Release@61619 9b283d60-5439-405e-af05-b73fd8c4d996

---
+++ Silverlight3/Source/RiaClient.Tests/System.ComponentModel.DataAnnotations/RequiredAttributeTest.cs
@@ -0,0 +1,32 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.ComponentModel.DataAnnotations.Test {
+    [TestClass]
+    public class RequiredAttributeTest {
+        [TestMethod]
+        public void IsValid() {
+            RequiredAttribute required = new RequiredAttribute();
+
+            Assert.AreEqual(true, required.IsValid("abcd"));
+            Assert.AreEqual(true, required.IsValid("  ab  "));
+            Assert.AreEqual(true, required.IsValid(10));
+
+            Assert.AreEqual(false, required.IsValid(""));
+            Assert.AreEqual(false, required.IsValid("   "));
+            Assert.AreEqual(false, required.IsValid("\t"));
+            Assert.AreEqual(false, required.IsValid(null));
+        }
+
+        [TestMethod]
+        public void DefaultErrorMessage() {
+            RequiredAttribute required = new RequiredAttribute();
+            Assert.AreEqual(String.Format(CultureInfo.CurrentCulture, Resources.DataAnnotationsResources.RequiredAttribute_ValidationError, "SOME_NAME"), required.FormatErrorMessage("SOME_NAME"));
+        }
+    }
+}
---
+++ WindowsPhone7/PhoneToolkitSample/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+using System;
+using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace PhoneToolkitSample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigateTo(string page)
+        {
+            this.NavigationService.Navigate(new Uri(page, UriKind.Relative));
+        }
+
+        private void OnToggleSwitch(object sender, RoutedEventArgs e)
+        {
+            NavigateTo("/Samples/ToggleSwitchSample.xaml");
+        }
+
+        private void OnTransitions(object sender, RoutedEventArgs e)
+        {
+            NavigateTo("/Samples/TransitionsSample.xaml");
+        }
+
+        private void OnTiltEffect(object sender, RoutedEventArgs e)
+        {
+            NavigateTo("/Samples/TiltEffectSample.xaml");
+        }
+
+        private void OnPerformanceProgressBar(object sender, RoutedEventArgs e)
+        {
+            NavigateTo("/Samples/PerformanceProgressBarSample.xaml");
+        }
+
+        private void OnContextMenu(object sender, RoutedEventArgs e)
+        {
+            NavigateTo("/Samples/ContextMenuSample.xaml");
+        }
+
+        private void OnDateTimePicker(object sender, RoutedEventArgs e)
+        {
+            NavigateTo("/Samples/DateTimePickerSample.xaml");
+        }
+
+        private void OnWrapPanel(object sender, RoutedEventArgs e)
+        {
+            NavigateTo("/Samples/WrapPanelSample.xaml");
+        }
+
+        private void OnGestures(object sender, RoutedEventArgs e)
+        {
+            NavigateTo("/Samples/GestureSample.xaml");
+        }
+
+        private void OnAutoCompleteBox(object sender, RoutedEventArgs e)
+        {
+            NavigateTo("/Samples/AutoCompleteBoxSample.xaml");
+        }
+
+        private void OnListPicker(object sender, RoutedEventArgs e)
+        {
+            NavigateTo("/Samples/ListPickerSample.xaml");
+        }
+
+        private void OnLongListSelector(object sender, RoutedEventArgs e)
+        {
+            NavigateTo("/Samples/LongListSelectorSample.xaml");
+        }
+    }
+}
